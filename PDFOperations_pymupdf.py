5b3d62dcbf362bb6ec82547d037430efb425b2b8
derturtle <floriangraf-mit-e.de>
2025-09-24 17:58:17 +0200

    Judging panel is working, for one Testfile

--- PDFOperations_pymupdf.py
+++ PDFOperations_pymupdf.py
@@ -173,15 +173,6 @@ class PDFOperations:
     
     def read_pdf(self, pdf_file: str) -> bool:
         
-        rd_index = -1
-        
-        def _next_textpage():
-            if len(self._doc.pages()) < rd_index + 1:
-                rd_index += 1
-                return list(self._doc.pages())[rd_index].get_textpage()
-            else:
-                return None
-        
         # ---- File checks -----
         # use full path
         pdf_file = os.path.abspath(pdf_file)
@@ -214,6 +205,17 @@ class PDFOperations:
         
         self._analyse_result_report(page_dict)
         
+        #
+        
+        # loop over
+        for section_no, segment in enumerate(self._collection.sections, start=1):
+            # get competition information
+            findings, page_dict, _ = read_obj.next_data(self._pdf_values.competition_sequenz, self._header_pos)
+            
+            self._analyse_judging_panel(page_dict, section_no)
+            b=1
+        
+        
         a=1
        
         
@@ -221,30 +223,32 @@ class PDFOperations:
         
     def _analyse_result_report(self, page_dict: dict):
         
-
+        # ----- Create variables
+        # Variable indicates if associations (0) should be found or clubs (1)
         parse_step: int = 0
-        
+        # shorter way to access config
         pdf_values = self._collection.config.pdf_values
-        
-        # Create variables
+        # Create an association object
         association: [Association, None] = None
-        
-        entry_len: int = 0
+        # Positon of the club in the array
         club_index: int = 0
+        # Store the header line (with club in it)
         header_line: list = []
-        
+        # Get key list
         keys = list(page_dict.keys())
+        
+        # ----- Find club index
         # Get entry len and club position
-        for i, key in enumerate(keys, start=0):
+        for key in keys:
             # Loop over list
-            for j in range(len(page_dict[key])):
-                if page_dict[key][j].text == pdf_values.club:
+            for i in range(len(page_dict[key])):
+                if page_dict[key][i].text == pdf_values.club:
                     header_line = page_dict[key]
-                    entry_len = len(page_dict[key])
-                    club_index = j
+                    club_index = i
                     break
-            if entry_len != 0:
-                break
+        # In case no header is found document wrong
+        if not header_line:
+            raise Exception(f'Value {pdf_values.club} not found until {" ".join([obj.text for obj in page_dict[list(page_dict.keys())[-1]]])}')
         
         # Do a debug print
         if club_index != 1:
@@ -252,9 +256,9 @@ class PDFOperations:
         else:
             print(fr'[{datetime.datetime.now().strftime("%H:%M:%S,%f")}] Debug | Clubs are numbered')
         
-        # Get first association an
+        # ----- Get associations and clubs
         for i, key in enumerate(keys, start=0):
-            # ----- Find assosiation
+            # ----- Find association
             if parse_step == 0:
                 if page_dict[key][club_index].text == pdf_values.club:
                     association_text = ' '.join(obj.text for obj in page_dict[keys[i-1]])
@@ -270,20 +274,85 @@ class PDFOperations:
             else:
                 # Create text from object
                 texts:  list = [obj.text for obj in page_dict[key]]
-                # No. and Club are the same
-                if club_index != 0 and len(page_dict[key]) == entry_len-1:
-                    split = texts[0].split('.')
-                    texts[0] = split[1]
-                    texts.insert(0, f'{split[0]}.')
+                # todo evaluate later (if still needed)
+                # # No. and Club are the same
+                # if club_index != 0 and len(page_dict[key]) == entry_len-1:
+                #     split = texts[0].split('.')
+                #     texts[0] = split[1]
+                #     texts.insert(0, f'{split[0]}.')
                 
                 if page_dict[keys[i+2]][club_index].text == pdf_values.club:
                     parse_step -= 1
                 else:
                     self._extract_club(page_dict[key][club_index:], header_line[club_index + 1].x, association)
-                # # if len(texts) == entry_len:
-                #     self._extract_club(page_dict[key][club_index:], header_line[club_index+1].y, association, texts[club_index])
-                # else:
-                #     parse_step -= 1
+        
+        # ----- Create Sections
+        for i, starts in enumerate(self._collection.clubs[0].starts_by_segments, start=1):
+            # Create Segments
+            Section(i)
+
+
+    def _analyse_judging_panel(self, page_dict: dict, section_no):
+        
+        end_points: list = []
+        page_list: list = []
+        
+        # Get first entry
+        values = page_dict[list(page_dict.keys())[0]]
+        
+        if len(values) != 3:
+            raise Exception(f"For judging panel three vales are expected. Found {values}")
+        
+        for value in values[1:]:
+            end_points.append(value.x)
+        # Add last point
+        #end_points.append(10000)
+        
+        # Create new list
+        for pdf_list in page_dict.values():
+            if pdf_list[0].text == self._pdf_values.segment:
+                # end loop
+                break
+            # Check if length is min the same otherwise reject value
+            if len(pdf_list) >= len(values):
+                indices = [0] * (len(end_points) + 2)
+                for i, end_point in enumerate(end_points):
+                    while indices[i+1] < len(pdf_list):
+                        if pdf_list[indices[i+1]].x >= end_point:
+                            indices[i + 2] = indices[i + 1] + 1
+                            break
+                        indices[i+1]+=1
+                
+                indices[len(indices)-1] = len(pdf_list)
+                tmp: list = []
+                for i in range(len(indices)-1):
+                    if indices[i] != indices[i+1]:
+                        tmp.append(PDFText.combine(pdf_list[indices[i]:indices[i+1]]))
+                    else:
+                        tmp.append(None)
+                
+                # if tmp is not text of position
+                if tmp[0].text != values[0].text and tmp[2] is not None:
+                    page_list.append(tmp)
+        
+        # ----- Create judging panel
+        for entry in page_list:
+            # Add club
+            club = self._collection.club_by_name(str(entry[2]))
+            if club is not None:
+                club.add_occurrence(entry[2])
+            else:
+                # club didn't exist
+                club = self._generate_club(entry[2])
+            # Add judge
+            if not entry[1]:
+                Judge(entry[0].text, '-', club, self._collection.sections_by_no(section_no))
+            else:
+                Judge(entry[0].text, entry[1].text, club, self._collection.sections_by_no(section_no))
+                
+                
+        
+
     
     def _extract_club(self, text_obj_line: list, x_end: float, association: Association) -> Club:
         """ Extract the club from a text line
@@ -312,21 +381,11 @@ class PDFOperations:
         # Create participants
         participants = [int(str(text_obj_line[i+1]).replace('/', '')), int(str(text_obj_line[i+2]).replace('/', ''))]
         club.participants = Participants(participants)
-        
+        # Create segments
         for j in range(i+3, len(text_obj_line)-2, 2):
             tmp = [int(str(text_obj_line[j]).replace('/', '')), int(str(text_obj_line[j+1]).replace('/', ''))]
             club.starts_by_segments.append(Starts(tmp))
         
-        # tmp = []
-        # for part in text_obj_line[2].text.split('/'):
-        #     tmp.append(int(part.strip()))
-        # club.participants = Participants(tmp)
-        # # Add segments
-        # for i in range(3, len(text_obj_line) - 1):
-        #     tmp = []
-        #     for part in text_obj_line[i].text.split('/'):
-        #         tmp.append(int(part.strip()))
-        #     club.starts_by_segments.append(Starts(tmp))
         return club
     
     def _generate_club(self, text_obj: PDFText, club_id: str = '-', name: str = '') -> Club:
